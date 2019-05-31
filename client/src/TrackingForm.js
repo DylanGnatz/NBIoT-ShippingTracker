5263a04887d181b112a9657432ec6991abe536ac
Dylan Gnatz <gnatzd@outlook.com>
2019-05-31 15:34:34 -0700

    began building front end React.js front-end interface

---
+++ client/src/TrackingForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class TrackingForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    axios
+      .get("http://localhost:3000/getEvents/" + this.state.value)
+      .then(response => console.log(response));
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the NB-IoT ShipTracker!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            SIM ID:
+            <br />
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <br />
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default TrackingForm;
