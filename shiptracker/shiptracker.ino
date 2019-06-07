0054d84be574f96ec9119ca1b7546dc65658c6f2
Dylan Gnatz <gnatzd@outlook.com>
2019-06-06 17:06:09 -0700

    updated arduino sketch

--- shiptracker/shiptracker.ino
+++ shiptracker/shiptracker.ino
@@ -7,7 +7,7 @@
 
 static const char *device_purpose = "Monitor GPS and environmental factors for a shipment";
 
-static const char *psk_key ="e938f2a8c1ccd407cce1293cbc707f0b";
+static const char *psk_key ="xxxxxxx";
 
 Breakout *breakout = &Breakout::getInstance();
 
