152d413b6b6a79b92ef3ac12cfb9c90d41d790e3
Dylan Gnatz <gnatzd@outlook.com>
2019-06-20 15:31:48 -0700

    added card containers for graphs

--- client/src/GraphView.js
+++ client/src/GraphView.js
@@ -3,6 +3,9 @@ import { XYPlot, LineSeries } from "react-vis";
 import Chart from "./Chart";
 import HumChart from "./HumChart.js";
 import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 class GraphView extends Component {
   constructor(props) {
@@ -25,8 +28,30 @@ class GraphView extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.renderGraph ? <Chart data={this.state.events} /> : null}
-        {this.state.renderGraph ? <HumChart data={this.state.events} /> : null}
+        <Container-Fluid>
+          <Row>
+            <Col>
+              <Card bg="primary" text="white" style={{}}>
+                <Card.Body>
+                  <Card.Title>Temperature vs. Time</Card.Title>
+                  {this.state.renderGraph ? (
+                    <Chart data={this.state.events} />
+                  ) : null}
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card bg="danger" text="white" style={{}}>
+                <Card.Body>
+                  <Card.Title>Humidity vs. Time</Card.Title>
+                  {this.state.renderGraph ? (
+                    <HumChart data={this.state.events} />
+                  ) : null}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container-Fluid>
       </div>
     );
   }
